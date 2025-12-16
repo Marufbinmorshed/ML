932241dd683579576d67dbda805dbcdcd47f52e5
Marufbinmorshed <161148584+Marufbinmorshed@users.noreply.github.com>
2025-12-16 12:07:22 +0600

    Add time conversion from seconds to hours, minutes, seconds

--- python/Day01/day 01/The Modulo Architect.py
+++ python/Day01/day 01/The Modulo Architect.py
@@ -1,1 +1,7 @@
-
+total=input("Enter a number:")
+total=int(total)
+hours=total//3600
+remaining_seconds=total%3600
+minutes=remaining_seconds//60
+seconds=remaining_seconds%60
+print(f"hours:{hours} , minutes:{minutes} , seconds:{seconds}")
