d9051a1be7afe65a23c12a622bf1c0575967fe4d
MD Maruf <161148584+Marufbinmorshed@users.noreply.github.com>
2025-12-17 17:04:02 +0600

    Add infinite loop for password input with exit option

--- python/Day_03 /The_Infinite_Guardian.py
+++ python/Day_03 /The_Infinite_Guardian.py
@@ -1,1 +1,7 @@
+while True:
+    password = input("Enter password (type 'stop' to exit): ")
+    if password == "stop":
+        print("Access terminated.")
+        break
+    print("Incorrect password. Try again.")
 
