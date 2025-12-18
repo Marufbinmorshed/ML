12cd46395bb247087b28aa7e83a2348672ae1cb3
MD Maruf <161148584+Marufbinmorshed@users.noreply.github.com>
2025-12-18 22:59:02 +0600

    Add Python Developer Roadmap to Readme
    Added a new roadmap for Python development and a description.

--- Readme.md.py
+++ Readme.md.py
@@ -1,1 +1,2 @@
-
+🚀 120-Day Python Developer Roadmap: Zero to Job-Ready
+A disciplined, assessment-driven journey to mastering Python, Machine Learning, and Software Engineering. This repository serves as a living portfolio of my technical growth, consistency, and professional readiness.
