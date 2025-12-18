16bcb0dce76d6f49f0573fbbf14b05eed2597b33
MD Maruf <161148584+Marufbinmorshed@users.noreply.github.com>
2025-12-18 12:35:13 +0600

    practise file Day04

---
+++ python/practise file Day04/The_Floating_point_trap.py
@@ -0,0 +1,52 @@
+# if data:
+#     print("Data Found")
+# else:
+#     print("No Data")
+
+# # Example with a 'truthy' value:
+# data_truthy = [1, 2, 3]
+# if data_truthy:
+#     print("Data Found")
+# else:
+#     print("No Data")
+
+# values=input("input something : ")
+# if values:
+#     print("Data Found")
+# else:
+#     print("No Data Found")
+    
+# score=int(input("Give Your Mark"))
+
+# if score > 70:
+#     print("Pass")
+# else:
+#     print("Fail")
+
+# score = 65
+# status = "Pass" if score > 70 else "Fail"
+# print(f"Score: {score}, Status: {status}")
+
+# score= 85
+# Result = "pass" if score >84 else "Fail"
+# print(f"score:{score}  Result:{Result}")
+# Result = (0.1 + 0.2 == 0.3)
+# print(f"Is 0.1 + 0.2 == 0.3 {Result}")
+# user = None
+
+# if user is not None and user.has_admin:
+#     print("Access granted")
+# data=[]
+# if data:
+#     print("Data Found")
+
+# else:
+#     print("No Data")
+# values=[]
+# if values:
+#     print("Data Found")
+# else:
+#     print("No Data Found")
+
+bill=float(input())
+print(f"${bill:,.3f}")
