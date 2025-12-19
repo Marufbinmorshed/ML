0d69931b6c1d57682dd73695f5e8601f1ecc8726
MD Maruf <161148584+Marufbinmorshed@users.noreply.github.com>
2025-12-19 16:03:23 +0600

    Add list and set lookup examples

--- python/Day_05/The Speed Trap (Lookup).py
+++ python/Day_05/The Speed Trap (Lookup).py
@@ -1,1 +1,18 @@
+#  list 
+numbers_list = list(range(10))   # imagine this is 1,000,000 numbers
 
+#  set 
+numbers_set = set(numbers_list)
+
+# Number to check
+target = -1
+
+# List lookup
+found_in_list = target in numbers_list
+
+print("Found in list:", found_in_list)
+
+# Set lookup
+found_in_set = target in numbers_set
+
+print("Found in set:", found_in_set)
