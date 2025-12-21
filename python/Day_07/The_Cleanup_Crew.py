8afa77525d52ed5a9533754365aaf9efe338c9ac
MD Maruf <161148584+Marufbinmorshed@users.noreply.github.com>
2025-12-21 09:34:23 +0600

    Update The_Cleanup_Crew.py

--- python/Day_07/The_Cleanup_Crew.py
+++ python/Day_07/The_Cleanup_Crew.py
@@ -1,1 +1,10 @@
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+     print(a / b)
+except Zero Division:
+    print("Division by zero is not allowed")
+except Value Error:
+    print("Please enter valid numbers")
+finally:
+    print("Execution Complete")
 
