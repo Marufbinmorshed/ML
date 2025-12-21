976ba44fea3f92bfc5475e248530924cf946d8de
MD Maruf <161148584+Marufbinmorshed@users.noreply.github.com>
2025-12-21 09:46:31 +0600

    Update The_Custom_Signal.py

--- python/Day_07/The_Custom_Signal.py
+++ python/Day_07/The_Custom_Signal.py
@@ -1,1 +1,9 @@
-
+try:
+    num = int(input("Enter a number: "))
+    if num < 0:
+        raise ValueError("No negatives")
+    print("You entered:", num)
+except ValueError as ddd:
+    print("Error:", ddd)
+finally:
+    print("Thanks's Bro")
