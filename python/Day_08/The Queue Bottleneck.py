bd36173dc5128cc3aa39deb35b2346faa9a420d4
MD Maruf <161148584+Marufbinmorshed@users.noreply.github.com>
2025-12-23 17:39:04 +0600

    Update The Queue Bottleneck.py

--- python/Day_08/The Queue Bottleneck.py
+++ python/Day_08/The Queue Bottleneck.py
@@ -1,1 +1,12 @@
+from collections import deque
+
+# Using list
+lst = list(range(100_000))
+lst.pop()     
+
+# Using deque
+dq = deque(range(100_000))
+dq.popleft()  
+
+print("Queue operation completed")
 
