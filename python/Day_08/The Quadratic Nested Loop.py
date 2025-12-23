acb348598d41c8d1161a96f36a0dab99151ae8e3
MD Maruf <161148584+Marufbinmorshed@users.noreply.github.com>
2025-12-23 17:49:18 +0600

    Update The Quadratic Nested Loop.py

--- python/Day_08/The Quadratic Nested Loop.py
+++ python/Day_08/The Quadratic Nested Loop.py
@@ -1,1 +1,12 @@
+list_a = list(range(10_000))
+list_b = list(range(5_000, 15_000))
+
+duplicates = []
+
+for a in list_a:
+    for b in list_b:
+        if a == b:
+            duplicates.append(a)
+
+print("Duplicates:", len(duplicates))
 
