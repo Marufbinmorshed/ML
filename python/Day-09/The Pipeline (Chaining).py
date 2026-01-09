9c4126dd77460271d6beb942606d0b8969804882
MD Maruf <161148584+Marufbinmorshed@users.noreply.github.com>
2026-01-09 19:56:51 +0600

    Update The Pipeline (Chaining).py

--- python/Day-09/The Pipeline (Chaining).py
+++ python/Day-09/The Pipeline (Chaining).py
@@ -1,1 +1,18 @@
+def numbers(n):
+    for i in range(n):
+        yield i
+
+def square(nums):
+    for n in nums:
+        yield n * n
+
+def even(nums):
+    for n in nums:
+        if n % 2 == 0:
+            yield n
+
+pipeline = even(square(numbers(10)))
+
+for value in pipeline:
+    print(value)
 
