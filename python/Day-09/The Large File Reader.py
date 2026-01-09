ddbf579e619c5e85a280a2da9f847d6858ecc73d
MD Maruf <161148584+Marufbinmorshed@users.noreply.github.com>
2026-01-09 19:57:11 +0600

    Update The Large File Reader.py

--- python/Day-09/The Large File Reader.py
+++ python/Day-09/The Large File Reader.py
@@ -1,1 +1,9 @@
+def fake_large_file():
+    for i in range(1_000_000):
+        yield f"Line {i}"
+
+for line in fake_large_file():
+    if int(line.split()[1]) == 5:
+        print(line)
+        break
 
