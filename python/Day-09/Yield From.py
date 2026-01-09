60c917f666ab87ee9d74046a83528df6de8e489d
MD Maruf <161148584+Marufbinmorshed@users.noreply.github.com>
2026-01-09 19:57:28 +0600

    Update Yield From.py

--- python/Day-09/Yield From.py
+++ python/Day-09/Yield From.py
@@ -1,1 +1,15 @@
+def gen_a():
+    yield 1
+    yield 2
+
+def gen_b():
+    yield 3
+    yield 4
+
+def main_gen():
+    yield from gen_a()
+    yield from gen_b()
+
+for x in main_gen():
+    print(x)
 
