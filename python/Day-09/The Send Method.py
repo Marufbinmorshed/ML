d7cd96d49881c22c3d10a6ac37af605e79ac26a7
MD Maruf <161148584+Marufbinmorshed@users.noreply.github.com>
2026-01-09 19:57:48 +0600

    Update The Send Method.py

--- python/Day-09/The Send Method.py
+++ python/Day-09/The Send Method.py
@@ -1,1 +1,10 @@
+def receiver():
+    value = yield
+    while True:
+        value = yield value * 2
 
+gen = receiver()
+next(gen)          # Prime the generator
+
+print(gen.send(10))
+print(gen.send(5))
