9ea13f751af87d73619baf59df2e32fc8e51efc9
MD Maruf <161148584+Marufbinmorshed@users.noreply.github.com>
2026-01-10 16:24:12 +0600

    Update The Manual Wrapper.py

--- python/Day_10/The Manual Wrapper.py
+++ python/Day_10/The Manual Wrapper.py
@@ -1,1 +1,13 @@
+def wrapper(func):
+    def inner():
+        print("Before function")
+        func()
+        print("After function")
+    return inner
+
+def say_hello():
+    print("Hello!")
+
+new_func = wrapper(say_hello)
+new_func()
 
