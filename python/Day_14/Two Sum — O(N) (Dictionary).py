e3fe283e312dfa96c0c6f775dd71e7c605008dde
MD Maruf <161148584+Marufbinmorshed@users.noreply.github.com>
2026-01-19 17:34:22 +0600

    Create Two Sum — O(N) (Dictionary).py

---
+++ python/Day_14/Two Sum — O(N) (Dictionary).py
@@ -0,0 +1,8 @@
+def two_sum(nums, target):
+    seen = {}
+    for i, num in enumerate(nums):
+        needed = target - num
+        if needed in seen:
+            return [seen[needed], i]
+        seen[num] = i
+    return []
